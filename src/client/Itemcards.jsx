9bd0c179dd23a6f11b8014bcb86d9467b021d2ef
Pavel-Kolev <146889340+Pavel-Kolev@users.noreply.github.com>
2023-11-28 23:04:13 +0200

    Update Itemcards.jsx
    added functionality to add to cart

--- src/client/Itemcards.jsx
+++ src/client/Itemcards.jsx
@@ -1,24 +1,40 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { ReactDOM } from "react";
+import { CartContext } from "./CartContext";
+import { useRef } from "react";
+import "../public/styles.css"
+ 
 import "../public/styles.css"
 
-import "../public/styles.css"
-function Itemcard(productprop){
+
+function Itemcard({_id,brand,model,disc,price,img}){
+    const inputEl = useRef(null);
+    const context= useContext(CartContext)
+    const {addOneToCart}= context.contextValue
+    const addToCartHandler=()=>{
+
+      addOneToCart(_id)
+       
+       }
  
-    
-console.log(productprop.productprop)
+
     return( <>
        
-<div className="Item">
-<a></a>
- <img className="product-photo" src={productprop.productprop.img}/>
+<div className="Item" >
+<a ></a>
+<p>{_id}</p>
+ <img className="product-photo" src={img}/>
  <div className="discription">
- <p className="product-text">{productprop.productprop.brand}</p>
- <p className="product-text">{productprop.productprop.disc}</p>
+ <p className="product-text">{ brand}</p>
+ <p className="product-text">{ disc}</p>
  </div>
- <p className="product-text">{productprop.productprop.model}</p>
- <p className="product-text">{productprop.productprop.price}$</p>
+ <p className="product-text">{  model}</p>
+ <p className="product-text">{ price}$</p>
+ <div>
+      <button onClick={addToCartHandler}>Add</button>
+     </div>
  </div>
+     
      
 </>
     )
