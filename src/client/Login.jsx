52468e0e1807572c069900605233e57b227d723f
Pavel-Kolev <146889340+Pavel-Kolev@users.noreply.github.com>
2023-11-28 23:03:43 +0200

    Create Login.jsx
    placeholder login page

---
+++ src/client/Login.jsx
@@ -0,0 +1,23 @@
+export default function Login(){
+
+
+
+
+    return (
+            <form action="">
+                <label htmlFor="username">username</label>
+                <input type="text"  name="username" id="username"/>
+                <label htmlFor="password">password</label>
+                <input type="text" name="password" id="password" />
+                <label htmlFor="email">email</label>
+                <input type="text" name="email" id="email"/>
+                <input type="submit" />
+
+
+
+
+            </form>
+
+
+    )
+}
