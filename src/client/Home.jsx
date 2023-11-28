71b2a341447205cc79dd3ec485e71ed00f2a3354
Pavel-Kolev <146889340+Pavel-Kolev@users.noreply.github.com>
2023-11-28 23:00:20 +0200

    Create Home.jsx
    basic home page without styling

---
+++ src/client/Home.jsx
@@ -0,0 +1,16 @@
+
+
+export default function Home(){
+
+
+return(
+
+<>
+<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus, facilis nulla voluptate vitae maiores ratione. Numquam enim obcaecati porro nemo inventore laudantium dolorum! Dolor repellat, maxime nulla praesentium odit corporis ea tempora perferendis distinctio reiciendis vero voluptates error illo nam quod officiis nisi vel, quam magni voluptas iure voluptatem. Culpa.</p>
+</>
+
+
+
+)
+
+}
