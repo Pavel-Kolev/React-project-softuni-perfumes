fa9a70188e88dd3fe143a1f99ed9d55482ea812c
Pavel-Kolev <146889340+Pavel-Kolev@users.noreply.github.com>
2023-11-28 23:02:22 +0200

    Create validateUsers.js
    a function to check credentials for errors

---
+++ src/client/utils/validateUsers.js
@@ -0,0 +1,23 @@
+function validateRegCredentials(values){
+const {email,username,password}=values
+let errors=[]
+const userRegex=/[^A-Za-z||0-9]/
+const emailRegex=/([A-Za-z0-9@]{10,})/
+const emailValid=emailRegex.test(email)
+
+
+if(!emailValid)
+{
+errors.push("Email Should include @ alphabet chars,numbers and exclude whitespaces")
+}
+if(username.length<8|| userRegex.test(username)===true){
+errors.push("The Username Should be atleast 8 char long and contain only letter or numbers")
+}
+ 
+if(password.length<6){
+    errors.push("Password  should be atleast 6 char long")
+}
+
+return(errors)
+}
+export default validateRegCredentials
