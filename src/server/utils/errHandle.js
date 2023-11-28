016fc8c6c09eefa3c2fd552152685da675e69303
Pavel-Kolev <146889340+Pavel-Kolev@users.noreply.github.com>
2023-11-28 22:54:30 +0200

    Create errHandle.js
    added mongoose error handling

---
+++ src/server/utils/errHandle.js
@@ -0,0 +1,10 @@
+const { MongooseError } = require("mongoose")
+
+exports.exactErrorMsg=(error)=>{
+   const isInstanceOfMongoose= error instanceof MongooseError
+   if(isInstanceOfMongoose){
+    const errors=Object.values(error.errors)
+    return errors.map(e=>e.message)
+   }
+    return[error.message]
+}
