616e28e450a2bf88ee67c09d9f9a18b0cc45afa6
Pavel-Kolev <146889340+Pavel-Kolev@users.noreply.github.com>
2023-12-01 10:56:34 +0200

    auth
    created and included authcontext thats sets on succesful login

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -5,18 +5,21 @@ import Navbar from "./Navbar";
 import * as userService from "./services/userService";
 import ProductSection from "./ProductSection";
 import { CartContext } from "./CartContext";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import Register from "./Register";
 import Login from "./Login";
 import Cart from "./cart";
 import Home from "./Home";
 import { ContextProvider } from "./CartContext";
 import * as perfumeService from "./services/perfumeService";
+import AuthContext from "./contexts/AuthContext";
  
 
 function App() {
-  const [err, setErr] = useState({});
- 
+  const [auth,setAuth]=useState([{}])
+  const navigate=useNavigate()
+  const [err, setErr] = useState([]);
+ const [loginErrs,setLoginErrs]=useState([])
   const registerSubmitHandler = async (values) => {
      
       const result = await userService.register(values)
@@ -25,18 +28,27 @@ function App() {
       }
      
     else{
-
+      setErr()
     }
+};
+const loginSubmitHandler = async (values) => {
      
-   
-
-     
-     
-    
-  };
-  
+  const result = await userService.login(values)
+  if(result.errorMessages){
+    setLoginErrs(result.errorMessages)
+  }
+ 
+else{
+setLoginErrs()
+setAuth(result)
+navigate("/")
+}
+console.log(auth)
+};
+ 
  
   return (
+    <AuthContext.Provider>
     <ContextProvider>
       <>
         <Navbar />
@@ -49,11 +61,12 @@ function App() {
             path="/register"
             element={<Register registerSubmitHandler={registerSubmitHandler}   arr={err}  />}
           ></Route>
-          <Route path="/login" element={<Login />}></Route>
+          <Route path="/login" element={<Login loginSubmitHandler={loginSubmitHandler} errs={loginErrs} />}></Route>
           <Route path="/cart" element={<Cart />}></Route>
         </Routes>
       </>
     </ContextProvider>
+    </AuthContext.Provider>
   );
 }
 
