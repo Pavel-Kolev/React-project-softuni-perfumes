a109d409d72dc0b0d178f133db4fd35c06b03fc0
Pavel-Kolev <146889340+Pavel-Kolev@users.noreply.github.com>
2023-12-01 09:20:35 +0200

    Update Login.jsx
    login component

--- src/client/Login.jsx
+++ src/client/Login.jsx
@@ -1,16 +1,31 @@
-export default function Login(){
+import { useEffect } from "react"
+import { useForm } from "./hooks/useForm"
+import * as userService from "./services/userService"
+
+export default function Login({loginSubmitHandler,errs}){
+    const{values,onSubmit,onChange,errors}= useForm(loginSubmitHandler,{
+        email:"",
+        username:"",
+        password:"",
+    },[],"login")
+
+useEffect(()=>{
 
 
 
+
+},errs)
 
     return (
-            <form action="">
+            <form onSubmit={onSubmit}>
+                {errs&&errs.map((error)=><p>{error}</p>)}
+                   <label htmlFor="email">email</label>
+                <input type="text" name="email" id="email"  onChange={onChange} value={values.email}  />
                 <label htmlFor="username">username</label>
-                <input type="text"  name="username" id="username"/>
+                <input type="text"  name="username" id="username" onChange={onChange} value={values.username}/>
                 <label htmlFor="password">password</label>
-                <input type="text" name="password" id="password" />
-                <label htmlFor="email">email</label>
-                <input type="text" name="email" id="email"/>
+                <input type="text" name="password" id="password"  onChange={onChange} value={values.password}/>
+             
                 <input type="submit" />
 
 
