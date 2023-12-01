539e5193c6546230ac86caf9e74f87961296e033
Pavel-Kolev <146889340+Pavel-Kolev@users.noreply.github.com>
2023-12-01 09:19:31 +0200

    made regex for email more concise

--- src/client/utils/validateUsers.js
+++ src/client/utils/validateUsers.js
@@ -2,20 +2,20 @@ function validateRegCredentials(values){
 const {email,username,password}=values
 let errors=[]
 const userRegex=/[^A-Za-z||0-9]/
-const emailRegex=/([A-Za-z0-9@]{10,})/
+const emailRegex=/[A-Za-z0-9]{5,}[@][a-z]*[.][a-z]*/gm
 const emailValid=emailRegex.test(email)
 
 
 if(!emailValid)
 {
-errors.push("Email Should include @ alphabet chars,numbers and exclude whitespaces")
+errors.push( "Email Should include @ alphabet chars,numbers and exclude whitespaces" )
 }
 if(username.length<8|| userRegex.test(username)===true){
-errors.push("The Username Should be atleast 8 char long and contain only letter or numbers")
+errors.push( "The Username Should be atleast 8 char long and contain only letter or numbers" )
 }
  
 if(password.length<6){
-    errors.push("Password  should be atleast 6 char long")
+    errors.push( "Password should be atleast 6 char long" )
 }
 
 return(errors)
