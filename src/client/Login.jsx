770baf40a5d24a999596c1ed014ba070726b68c9
Pavel-Kolev <146889340+Pavel-Kolev@users.noreply.github.com>
2023-12-01 10:55:43 +0200

    Update Login.jsx
    added toast component for errors

--- src/client/Login.jsx
+++ src/client/Login.jsx
@@ -1,6 +1,10 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useForm } from "./hooks/useForm"
 import * as userService from "./services/userService"
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Toast from 'react-bootstrap/Toast';
 
 export default function Login({loginSubmitHandler,errs}){
     const{values,onSubmit,onChange,errors}= useForm(loginSubmitHandler,{
@@ -8,17 +12,30 @@ export default function Login({loginSubmitHandler,errs}){
         username:"",
         password:"",
     },[],"login")
+    const[showErr , setShow]=useState(true)
+ 
+const ErrorHandleShow=()=>{
+    if(errs||errors){
+        setShow(true)
+    }
+  
+else{
+    setShow(false)
+}
 
-useEffect(()=>{
+} 
+const ErrorHandleCloser=()=>{
+    setShow(false)
+    
+    
+    }
 
-
-
-
-},errs)
-
+    
+ 
     return (
+        <>
             <form onSubmit={onSubmit}>
-                {errs&&errs.map((error)=><p>{error}</p>)}
+              
                    <label htmlFor="email">email</label>
                 <input type="text" name="email" id="email"  onChange={onChange} value={values.email}  />
                 <label htmlFor="username">username</label>
@@ -26,13 +43,29 @@ useEffect(()=>{
                 <label htmlFor="password">password</label>
                 <input type="text" name="password" id="password"  onChange={onChange} value={values.password}/>
              
-                <input type="submit" />
+                <input type="submit" onClick={ErrorHandleShow}/>
 
 
 
 
             </form>
-
-
+         <div className="toast-container">
+  {errs&&errs.map((error)=>  
+        
+        <Toast show={showErr} onClose={ErrorHandleCloser} key={error}>
+          <Toast.Header>
+            <img
+              src="holder.js/20x20?text=%20"
+              className="rounded me-2"
+              alt=""
+            />
+            <strong className="me-auto">Bootstrap</strong>
+            <small>11 mins ago</small>
+          </Toast.Header>
+          <Toast.Body>{error}</Toast.Body>
+        </Toast>
+     )}
+  </div>
+  </>
     )
 }
