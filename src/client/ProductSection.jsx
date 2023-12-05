21c2a5ffa46b49114b5282a35fd990a5981ccdb9
Pavel-Kolev <146889340+Pavel-Kolev@users.noreply.github.com>
2023-12-06 00:28:24 +0200

    Update ProductSection.jsx
    every rerender will cause a new request so we get the freshest info

--- src/client/ProductSection.jsx
+++ src/client/ProductSection.jsx
@@ -2,25 +2,30 @@ import React from "react";
 import { useState } from 'react'
  import { useEffect } from 'react'
  import { useContext } from "react";
-  
+ import * as perfumeService from"./services/perfumeService"
 import Itemcard from "./Itemcards";
 import { CartContext } from "./CartContext";
 
-function ProductSection(){
-        const context= useContext(CartContext)
-        const {items,loading}= context.contextValue
 
+function ProductSection(){
+        
+        const [cartProducts, setCartProducts] = useState();
+       useEffect(()=>{
+        perfumeService.getAllParfumes().then(productwithquant=>(productwithquant.map((cartProducts)=> ({...cartProducts , quantity:0})   ))).then(productswithquant=>setCartProducts(productswithquant))
+       },[])
+        
+      
         return(
     <>
             <div className='product-section'>
 
-{items&&items.map((product)=>
+{cartProducts&&cartProducts.map((product)=>
         <Itemcard key={product._id} {...product}></Itemcard>)}
 
 
              
              </div>
-             {!items   && <h3>No Items posted yet... Be the initiator</h3>}
+             {!cartProducts  && <h3>No Items posted yet... Be the initiator</h3>}
              </>
         )
       
