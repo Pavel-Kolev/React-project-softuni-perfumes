a237ecc86da372bacf5382ffa41fab0065bc3530
Pavel-Kolev <146889340+Pavel-Kolev@users.noreply.github.com>
2023-12-06 00:23:24 +0200

    Update parfumeServices.js
    added create parfume funct
    added get items of user funct
    added get items of nonuser funct

--- src/server/services/parfumeServices.js
+++ src/server/services/parfumeServices.js
@@ -5,3 +5,23 @@ exports.getAll = async () => {
      
     return parfumes;
   };
+  exports.create = async (data) => {
+    const parfume = await parfumeee.create(data);
+  
+    return parfume;
+  };
+  exports.getParfumesOfUser =async(userId)=>{
+const parfume =await parfumeee.find({owner:userId}).lean()
+ 
+return parfume
+
+
+  }
+  exports.getParfumesOfNonUser =async(owners)=>{
+    const parfume =await parfumeee.find().lean()
+const parfumes= parfume.filter((product)=>product.owner!=owners)
+  
+    return parfumes
+    
+    
+      }
