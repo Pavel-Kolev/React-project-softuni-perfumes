bfe17e2516331d928f184384866795f614126c31
Pavel-Kolev <146889340+Pavel-Kolev@users.noreply.github.com>
2023-12-06 00:31:30 +0200

    Update CartItemDetails.jsx
    added handler for removing from cart

--- src/client/CartItemDetails.jsx
+++ src/client/CartItemDetails.jsx
@@ -1,36 +1,41 @@
 
-import { useContext,useEffect } from "react";
+import { useContext,useEffect, useState } from "react";
 import { Button } from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "../public/cart.css"
  
 import { CartContext } from "./CartContext";
 
-export default function CartItemDetails(product){
-useEffect(()=>{
-    const{_id,brand,model,disc,price,img}=product
-},[])
+export default function CartItemDetails({_id,brand,model,disc,price,img,quantity}){
+    const context= useContext(CartContext)
+        const {deleteFromCart}= context.contextValue
+ const [show,setShow]=useState(true)
    
+const removeFromCartHandler=()=>{
+deleteFromCart(_id)
+setShow(false)
 
+}
 return(<>
-    <tr>
-          <td><img src={product.img}/></td>
-          <td>{product.brand} {product.disc}</td>
+{show&&<tr>
+          <td><img src={img}/></td>
+          <td>{brand} {disc}</td>
         
-          <td>  {product.quantity}</td>
-          <td>{product.price}</td>
+          <td>  {quantity}</td>
+          <td>{price}</td>
           <td>
             <div className="buttons-group">
                 <Button>add</Button>
                 <Button>reset</Button>
-                <Button>delete</Button>
+                <Button onClick={removeFromCartHandler}>delete</Button>
             </div>
 
           </td>
 
  </tr>
   
-           </>
+} </> 
+    
 )
 
 
