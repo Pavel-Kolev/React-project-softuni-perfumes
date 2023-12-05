f0c45c364767a48db8ab7d6c00b070bc448729f3
Pavel-Kolev <146889340+Pavel-Kolev@users.noreply.github.com>
2023-12-06 00:29:48 +0200

    Update request.js
    covered cases with and without token and data

--- src/client/lib/request.js
+++ src/client/lib/request.js
@@ -1,15 +1,42 @@
  
+
  const buildOptions=(data)=>{
 const options={}
- 
+const token=localStorage.getItem("token")
 if(data){
-    options.body=JSON.stringify(data)
+
+    options.body=JSON.stringify({data:data})
     options.headers={
         "content-type":"application/json",
      
     }
    
+ if(data&&token){
+   
+        options.body= JSON.parse(options.body)
+        
+    
+ 
+    options.body.token=token
+    options.body=JSON.stringify(options.body)
+    console.log(options.body)
 }
+
+
+     
+ }
+ if(!data&&token){
+  
+   options.body = JSON.stringify({token:token})
+ 
+  
+    options.headers={
+        "content-type":"application/json",
+     
+    }
+   console.log(options.body)
+     
+ }
  
 return options
  }
@@ -17,9 +44,9 @@ return options
  export const request =async(method,url,data)=>{
  
 const response =await fetch(url,{...buildOptions(data),method})
- 
+  
 const result=await response.json() 
-console.log(result)
+ 
 return result
  
  
