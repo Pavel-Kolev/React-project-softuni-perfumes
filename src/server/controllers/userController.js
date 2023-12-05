f9d26c6920c62091b90b90225b8ae63e9b99efd6
Pavel-Kolev <146889340+Pavel-Kolev@users.noreply.github.com>
2023-12-06 00:24:21 +0200

    Create userController.js
    moved endpoints from app to user controller

---
+++ src/server/controllers/userController.js
@@ -0,0 +1,51 @@
+const router = require("express").Router();
+const { auth, isAuthenticated } = require("../middlewares/authmiddleware");
+const userService=require("../services/userServices")
+router.post("/register",async (req,res)=>{
+    const {email,username,password}=req.body.data
+    try{  
+     const token=await userService.register(email,username,password)
+    
+     res.json({token,email,username})
+   } 
+    catch (err) {
+       
+       const{message}=err
+      
+       const errorMessages=message.split(",")
+       
+       res.status(400).json({errorMessages})
+      
+     }
+    
+    res.end()
+   
+   
+   })
+   router.post("/login", async (req, res) => {
+       
+       const { email,username, password } = req.body.data;
+     
+     
+   try{
+       const token = await userService.login(email,username, password);
+       
+      
+       res.json({token,email,username});
+     
+   }   
+       catch(err){
+           const{message}=err
+      
+       const errorMessages=message.split(",")
+       
+       res.status(400).json({errorMessages})
+      
+       }
+     
+       
+      
+    res.end()
+     });
+
+  module.exports = router;
