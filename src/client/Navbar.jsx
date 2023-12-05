535cfc5db37ed0f83fe8a598f5620efcec5f1e2e
Pavel-Kolev <146889340+Pavel-Kolev@users.noreply.github.com>
2023-12-06 00:28:36 +0200

    Update Navbar.jsx
    added authenticated routing

--- src/client/Navbar.jsx
+++ src/client/Navbar.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
 import { ReactDOM } from "react";
 import "../public/header.css"
 import {Link} from "react-router-dom"
+import AuthContext from "./contexts/AuthContext";
 function Navbar(){
-
+const {isAuthenticated,username}=useContext(AuthContext)
     return(
         <header>
             <nav>
@@ -13,10 +14,18 @@ function Navbar(){
        
         <li><Link to= "/">Home </Link></li>
         <li><Link to="/Catalog">Catalog</Link></li>
+        {!isAuthenticated&& <>
         <li><Link to="/register">Register</Link></li>
         <li><Link to="/login">Login </Link></li>
-        <li><Link to="/Cart">Cart </Link></li>
-         
+        </>
+         }
+         {isAuthenticated&& <> <li><Link to="/Logout">Logout </Link></li>
+         <li><Link to="/Create">Create </Link></li>
+         <li><Link to="/UserParfumes">My Offers</Link></li>
+           </>
+}
+           <li><Link to="/Cart">Cart </Link></li>
+       
         
       </ul>
       </nav>
