d568a2544cc7a931d7a80ec1f9688814fc926924
Pavel-Kolev <146889340+Pavel-Kolev@users.noreply.github.com>
2023-12-06 00:27:46 +0200

    Update Register.jsx
    added react bootstrap components  and moved registersubmithandler into context from props

--- src/client/Register.jsx
+++ src/client/Register.jsx
@@ -1,41 +1,95 @@
-import { useState } from "react"
+import { useContext, useState } from "react"
 import { useForm } from "./hooks/useForm"
 import * as userService from "./services/userService"
+import AuthContext from "./contexts/AuthContext"
+import Toast from 'react-bootstrap/Toast';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 
 
-
-export default function Register({registerSubmitHandler,arr} ) {
-  
+export default function Register({arr} ) {
+  const{registerSubmitHandler}=useContext(AuthContext)
 const{values,onSubmit,onChange,errors}= useForm(registerSubmitHandler,{
     email:"",
     username:"",
     password:"",
 },[],"register")
+const[showErr , setShow]=useState(true)
  
+const ErrorHandleShow=()=>{
+    if(arr||errors){
+        setShow(true)
+    }
+  
+else{
+    setShow(false)
+}
+
+} 
+const ErrorHandleCloser=()=>{
+    setShow(false)
+    
+    
+    }
  
  
     
 console
     return (
+      <>
+<Form onSubmit={onSubmit}>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" name="email" onChange={onChange} value={values.email} />
+      </Form.Group>
 
-            <form onSubmit={onSubmit}>
-                <label htmlFor="email">email</label>
-                <input type="text" name="email" onChange={onChange} value={values.email} placeholder="something@smth.smth"/>
-                {errors&&errors.map((error)=><p key={error}>{error}</p>
-                )}
-              {arr&&arr.map((error)=><p key={error}>{error}</p>)}
-                <label htmlFor="username"> Username</label>
-                <input type="text"  name="username" onChange={onChange} value={values.username} placeholder="something@smth.smth"/>
-                <label htmlFor="password">Password</label>
-                <input type="text" name="password" onChange={onChange} value={values.password} placeholder="something@smth.smth" />
-                <input type="submit"/>
+      <Form.Group className="mb-3" controlId="formBasicUsername">
+        <Form.Label>Username</Form.Label>
+        <Form.Control type="username" placeholder="Username" name="username" onChange={onChange} value={values.username} />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+      <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" name="password" onChange={onChange} value={values.password}/>
+      </Form.Group>
+      <Button variant="primary" type="submit" onClick={ErrorHandleShow}>
+        Submit
+      </Button>
+    </Form>
+    <div className="toast-container">
+                {errors&&errors.map((error)=>  
+        
+        <Toast show={showErr} onClose={ErrorHandleCloser} key={error}>
+          <Toast.Header>
+            <img
+              src="holder.js/20x20?text=%20"
+              className="rounded me-2"
+              alt=""
+            />
+            <strong className="me-auto">Bootstrap</strong>
+            <small>11 mins ago</small>
+          </Toast.Header>
+          <Toast.Body>{error}</Toast.Body>
+        </Toast>
+     )}
+                {arr&&arr.map((error)=>  
+        
+        <Toast show={showErr} onClose={ErrorHandleCloser} key={error}>
+          <Toast.Header>
+            <img
+              src="holder.js/20x20?text=%20"
+              className="rounded me-2"
+              alt=""
+            />
+            <strong className="me-auto">Bootstrap</strong>
+            <small>11 mins ago</small>
+          </Toast.Header>
+          <Toast.Body>{error}</Toast.Body>
+        </Toast>
+     )}
+     </div>
+            
 
-
-
-
-            </form>
-
-
+            </>
     )
 }
