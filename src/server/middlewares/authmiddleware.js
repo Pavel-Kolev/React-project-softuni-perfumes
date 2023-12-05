28463f1d41dbb390c5c957b07747724767fa5433
Pavel-Kolev <146889340+Pavel-Kolev@users.noreply.github.com>
2023-12-06 00:24:37 +0200

    Create authmiddleware.js
    created an auth middleware

---
+++ src/server/middlewares/authmiddleware.js
@@ -0,0 +1,44 @@
+const jwt = require("../lib/jwt");
+ 
+
+exports.auth = async (req, res, next) => {
+  
+ 
+ console.log(req.body.token)
+  const token =  req.body.token 
+  if (token) {
+    try{
+   
+        const decodeToken=await jwt.verify(token,"atanas")
+        
+        req.user=decodeToken
+        res.locals.user=decodeToken
+        res.locals.isAuthenticated=true;
+        
+    }
+    catch(error){
+        throw new Error(error)
+         
+         
+    }
+  
+  } 
+  else {
+    
+    next();
+  }
+
+ 
+ next();
+ 
+ 
+
+  
+};
+exports.isAuthenticated=(req,res,next)=>{
+    if(!req.user){
+    
+      res.locals.isAuthenticated=false;
+    }
+    next()
+}
