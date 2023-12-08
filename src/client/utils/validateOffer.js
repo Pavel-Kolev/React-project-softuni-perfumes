f4a8b9ca0f44fbaa469357fff1a3e84ce84ab063
Pavel-Kolev <146889340+Pavel-Kolev@users.noreply.github.com>
2023-12-08 04:14:14 +0200

    Create validateOffer.js
    util for validating input length and price

---
+++ src/client/utils/validateOffer.js
@@ -0,0 +1,15 @@
+function validateOffer(values){
+    const {brand,model,img,price,disc}=values
+    let errors=[]
+    
+   
+    if(brand.length<7||model.length<7||img.length<7||disc.length<7)
+    {
+    errors.push( "Provide input that is a bit longer" )
+    }
+    if(Number(price)>1500){
+        errors.push( "We are sorry, but such expensive products shoudn't be sold on here " )
+    }
+    return(errors)
+    }
+    export default validateOffer
