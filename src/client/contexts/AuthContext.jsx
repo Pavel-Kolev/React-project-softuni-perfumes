54b2f06471942541af7c9b3813d95e521062757e
Pavel-Kolev <146889340+Pavel-Kolev@users.noreply.github.com>
2023-12-08 04:18:58 +0200

    Update AuthContext.jsx

--- src/client/contexts/AuthContext.jsx
+++ src/client/contexts/AuthContext.jsx
@@ -1,5 +1,79 @@
 import { createContext } from "react";
-
+import { useState } from "react";
+import * as userService from "../services/userService";
 const AuthContext=createContext()
+import { useNavigate } from "react-router-dom";
+import * as parfumeService from "../services/perfumeService";
+import usePersistedState from "../hooks/usePersistedState";
 
+export const AuthProvider=({children})=>{
+    const [auth, setAuth] = usePersistedState('auth',{})
+    const navigate = useNavigate();
+    const [err, setErr] = useState([]);
+    const [loginErrs, setLoginErrs] = useState([]);
+  
+  
+  
+    const registerSubmitHandler = async (data) => {
+      const result = await userService.register(data);
+      if (result.errorMessages) {
+        setErr(result.errorMessages);
+      } else {
+        setErr();
+  
+        setAuth(result);
+        localStorage.setItem("token", result.token);
+        navigate("/");
+      }
+    };
+    const loginSubmitHandler = async (data) => {
+      const result = await userService.login(data);
+      if (result.errorMessages) {
+        setLoginErrs(result.errorMessages);
+      } else {
+     
+        setLoginErrs();
+        setAuth(result);
+        localStorage.setItem("token", result.token);
+        navigate("/");
+      }
+    };
+  
+    const logoutHandler = () => {
+      setAuth({});
+      localStorage.removeItem("token");
+      navigate("/");
+    };
+    const createOfferHandler = async (data) => {
+      const result = await parfumeService.CreateParfume(data);
+   
+      if (result.errorMessages) {
+        setErr(result.errorMessages);
+      } else {
+        setErr();
+        navigate("/");
+      }
+    };
+    
+   
+    const values = {
+      createOfferHandler,
+      registerSubmitHandler,
+      loginSubmitHandler,
+      logoutHandler,
+     
+      username: auth.username,
+      email: auth.email,
+      token: auth.token,
+      isAuthenticated: !!auth.token,
+      err:err,
+      loginErrs:loginErrs,
+      id:auth.id
+
+    };
+return(
+    
+    <AuthContext.Provider value={values}>{children}</AuthContext.Provider>
+)
+}
 export default AuthContext
