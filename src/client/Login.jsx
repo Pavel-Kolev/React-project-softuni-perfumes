1dbddb3e6c7d5bb400c4390f3b315c66c5b904b2
Pavel-Kolev <146889340+Pavel-Kolev@users.noreply.github.com>
2023-12-08 04:16:51 +0200

    Update Login.jsx
    renamed errs

--- src/client/Login.jsx
+++ src/client/Login.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from "react"
+import { useContext, useEffect, useState,useMemo } from "react"
 import { useForm } from "./hooks/useForm"
 import * as userService from "./services/userService"
 import Col from 'react-bootstrap/Col';
@@ -7,17 +7,18 @@ import Toast from 'react-bootstrap/Toast';
 import AuthContext from "./contexts/AuthContext";
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-export default function Login({errs}){
-    const{loginSubmitHandler}=useContext(AuthContext)
-    const{values,onSubmit,onChange,errors}= useForm(loginSubmitHandler,{
-        email:"",
-        username:"",
-        password:"",
-    },[],"login")
-    const[showErr , setShow]=useState(true)
- 
+export default function Login( ){
+    const{loginSubmitHandler,loginErrs}=useContext(AuthContext)
+    const initialValues=useMemo(()=>({
+      email:"",
+      username:"",
+      password:"",
+  }),[])
+    const{values,onSubmit,onChange,errors}= useForm(loginSubmitHandler,initialValues,[],"login")
+    const[showErr , setShow]=useState(false)
+    
 const ErrorHandleShow=()=>{
-    if(errs||errors){
+    if(loginErrs||errors){
         setShow(true)
     }
   
@@ -35,7 +36,7 @@ const ErrorHandleCloser=()=>{
     
  
     return (
-        <>
+        <><div className="container-form">
            <Form onSubmit={onSubmit}>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
@@ -54,8 +55,9 @@ const ErrorHandleCloser=()=>{
         Submit
       </Button>
     </Form>
+    </div>
          <div className="toast-container">
-  {errs&&errs.map((error)=>  
+  {loginErrs&&loginErrs.map((error)=>  
         
         <Toast show={showErr} onClose={ErrorHandleCloser} key={error}>
           <Toast.Header>
