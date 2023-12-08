234af4e90ef8eff60a1487b4c7eb3fad1262c461
Pavel-Kolev <146889340+Pavel-Kolev@users.noreply.github.com>
2023-12-08 04:16:20 +0200

    Update ProductSection.jsx
    renamed for more clarity

--- src/client/ProductSection.jsx
+++ src/client/ProductSection.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import { useState } from 'react'
  import { useEffect } from 'react'
- import { useContext } from "react";
+ 
  import * as perfumeService from"./services/perfumeService"
 import Itemcard from "./Itemcards";
-import { CartContext } from "./CartContext";
+ 
+ 
 
 
 function ProductSection(){
-        
-        const [cartProducts, setCartProducts] = useState();
+       
+        const [catalogProducts, setCartProducts] = useState();
        useEffect(()=>{
         perfumeService.getAllParfumes().then(productwithquant=>(productwithquant.map((cartProducts)=> ({...cartProducts , quantity:0})   ))).then(productswithquant=>setCartProducts(productswithquant))
        },[])
@@ -19,13 +20,13 @@ function ProductSection(){
     <>
             <div className='product-section'>
 
-{cartProducts&&cartProducts.map((product)=>
-        <Itemcard key={product._id} {...product}></Itemcard>)}
+{catalogProducts&&catalogProducts.map((product)=>
+        <Itemcard key={product._id} {...product}  ></Itemcard>)}
 
 
              
              </div>
-             {!cartProducts  && <h3>No Items posted yet... Be the initiator</h3>}
+             {!catalogProducts  && <h3>No Items posted yet... Be the initiator</h3>}
              </>
         )
       
