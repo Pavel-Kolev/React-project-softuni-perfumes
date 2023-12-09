e8433d68b2a0d97142cc161fd3c3be07f35108fa
Pavel-Kolev <146889340+Pavel-Kolev@users.noreply.github.com>
2023-12-09 07:53:23 +0200

    Create offerController.js
    controller for offers

---
+++ src/server/controllers/offerController.js
@@ -0,0 +1,55 @@
+const router = require("express").Router();
+const offerServices=require("../services/offerServices")
+const { auth, isAuthenticated } = require("../middlewares/authmiddleware");
+router.use(auth)
+router.post('/:id/create',(req,res)=>{
+    const {id}=req.params
+    const {additionalInfo,counterPrice}=req.body.data
+    const owner=req.user._id
+     
+    try{
+        offerServices.createBid(id,{additionalInfo,counterPrice,owner});
+          res.json("Counter-offers created")
+    }
+    catch(err){
+        const{message}=err
+      
+        const errorMessages=message.split(",")
+        
+        res.status(400).json({errorMessages})
+    }
+    res.end()
+})
+router.post('/:id/find',async (req,res)=>{
+    const {id}=req.params
+    const userid=req.user._id
+    try {
+      const result= await offerServices.find(id)
+       res.json(result)
+
+    } catch (error) {
+        const{message}=error
+      
+        const errorMessages=message.split(",")
+        
+        res.status(400).json({errorMessages})
+    }
+
+})
+router.delete('/:parfumeId/:id/delete',async (req,res)=>{
+    const {id}=req.params
+   const {parfumeId}=req.params
+    try {
+      const result= await offerServices.delete(id,parfumeId)
+       res.json(result)
+
+    } catch (error) {
+        const{message}=error
+      
+        const errorMessages=message.split(",")
+        
+        res.status(400).json({errorMessages})
+    }
+
+})
+module.exports=router
