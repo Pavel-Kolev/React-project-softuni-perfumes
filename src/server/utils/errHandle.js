301879466cee8ea153d51e8c20f6295ad4aeb21c
Pavel-Kolev <146889340+Pavel-Kolev@users.noreply.github.com>
2023-12-09 21:35:55 +0200

    Update errHandle.js

--- src/server/utils/errHandle.js
+++ src/server/utils/errHandle.js
@@ -1,10 +1,11 @@
 const { MongooseError } = require("mongoose")
 
 exports.exactErrorMsg=(error)=>{
+    console.log(error)
    const isInstanceOfMongoose= error instanceof MongooseError
    if(isInstanceOfMongoose){
     const errors=Object.values(error.errors)
     return errors.map(e=>e.message)
    }
-    return[error.message]
+ 
 }
