5c7085d0c6367a1b7d9ae199b47a4d2c4cf0a1b8
Pavel-Kolev <146889340+Pavel-Kolev@users.noreply.github.com>
2023-12-09 21:20:41 +0200

    placeholders
    added placeholders when the data is being loaded

--- src/client/ProductSection.jsx
+++ src/client/ProductSection.jsx
@@ -4,22 +4,27 @@ import { useState } from 'react'
  
  import * as perfumeService from"./services/perfumeService"
 import Itemcard from "./Itemcards";
- 
+import Spinner from 'react-bootstrap/Spinner';
+import CardLoading from "./CardLoading";
  
 
 
 function ProductSection(){
-       
+                const [loading,setLoading]=useState(true)
         const [catalogProducts, setCartProducts] = useState();
        useEffect(()=>{
-        perfumeService.getAllParfumes().then(productwithquant=>(productwithquant.map((cartProducts)=> ({...cartProducts , quantity:0})   ))).then(productswithquant=>setCartProducts(productswithquant))
+        perfumeService.getAllParfumes().then(setLoading(false)).then(productwithquant=>(productwithquant.map((cartProducts)=> ({...cartProducts , quantity:0})   ))).then(productswithquant=>setCartProducts(productswithquant))
+       
        },[])
         
       
         return(
     <>
-            <div className='product-section'>
+    
 
+
+            <div className='product-section'>
+            {loading&&(<CardLoading></CardLoading>)}
 {catalogProducts&&catalogProducts.map((product)=>
         <Itemcard key={product._id} {...product}  ></Itemcard>)}
 
