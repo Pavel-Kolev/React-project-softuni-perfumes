36f96ff9b9cf4427e0d6b9d38765483af10b4693
Pavel-Kolev <146889340+Pavel-Kolev@users.noreply.github.com>
2023-12-09 21:34:39 +0200

    Update CommentSender.jsx
    only authenticated users can send offers

--- src/client/CommentSender.jsx
+++ src/client/CommentSender.jsx
@@ -8,50 +8,49 @@ export default function CommentSender({onSubmit,values,ErrorHandleShow,onChange}
 const {isAuthenticated}=useContext(AuthContext)
 
 
-    return(<>
-        <div className="container-form-comment">
-                      <Form onSubmit={onSubmit}>
-                         
-                        
-                        <Form.Group
-                          className="mb-3"
-                          controlId="exampleForm.ControlTextarea1"
-                        >
-                          <Form.Label>
-                            Delivery details-addres,contancts and other
-                            specifications
-                          </Form.Label>
-                          <Form.Control
-                            as="textarea"
-                            onChange={onChange}
-                            name="additionalInfo"
-                            rows={3}
-                            value={values.additionalInfo}
-                          />
-                        </Form.Group>
-                        <Form.Group
-                          className="mb-3"
-                          controlId="exampleForm.ControlInput2"
-                        >
-                          <Form.Label>Counter-Offer</Form.Label>
-                          <Form.Control
-                            type="counterPrice"
-                            onChange={onChange}
-                            name="counterPrice"
-                            placeholder="100$"
-                            value={values.counterPrice}
-                          />
-                        </Form.Group>{isAuthenticated&&<Button
-                          variant="primary"
-                          type="submit"
-                          onClick={ErrorHandleShow}
-                        >
-                          Submit
-                        </Button>}
-                        {!isAuthenticated&&<p>Please log in to send Offers</p>}
-                        
-                      </Form>
-                    </div>
-                    </>
-    )
+    return(
+      <>{isAuthenticated&& <div className="container-form-comment">
+      <Form onSubmit={onSubmit}>
+         
+        
+        <Form.Group
+          className="mb-3"
+          controlId="exampleForm.ControlTextarea1"
+        >
+          <Form.Label>
+            Delivery details-addres,contancts and other
+            specifications
+          </Form.Label>
+          <Form.Control
+            as="textarea"
+            onChange={onChange}
+            name="additionalInfo"
+            rows={3}
+            value={values.additionalInfo}
+          />
+        </Form.Group>
+        <Form.Group
+          className="mb-3"
+          controlId="exampleForm.ControlInput2"
+        >
+          <Form.Label>Counter-Offer</Form.Label>
+          <Form.Control
+            type="counterPrice"
+            onChange={onChange}
+            name="counterPrice"
+            placeholder="100$"
+            value={values.counterPrice}
+          />
+        </Form.Group>{isAuthenticated&&<Button
+          variant="primary"
+          type="submit"
+          onClick={ErrorHandleShow}
+        >
+          Submit
+        </Button>}
+       
+        
+      </Form> </div>}
+  </>  )
+    
 }
