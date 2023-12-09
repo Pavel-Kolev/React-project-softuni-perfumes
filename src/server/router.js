9e9963fb6c2814c81b107eea7ee8ddb564050a8d
Pavel-Kolev <146889340+Pavel-Kolev@users.noreply.github.com>
2023-12-09 07:52:28 +0200

    Update router.js
    added router for offers

--- src/server/router.js
+++ src/server/router.js
@@ -1,12 +1,13 @@
 const router= require("express").Router();
 const userController=require('./controllers/userController')
 const parfumeController=require("./controllers/parfumeController")
+const offerController=require("./controllers/offerController")
 const { auth, isAuthenticated } = require("./middlewares/authmiddleware");
 
  
 router.use("/user",userController)
 router.use("/parfumes",parfumeController)
-
+router.use("/offers",offerController)
 
 
 
