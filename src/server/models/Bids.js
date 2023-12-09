b1832cda5208c99669a4fc024682f0cdd41832a0
Pavel-Kolev <146889340+Pavel-Kolev@users.noreply.github.com>
2023-12-09 07:53:14 +0200

    Update Bids.js
    made info and price required

--- src/server/models/Bids.js
+++ src/server/models/Bids.js
@@ -1,7 +1,8 @@
 const mongoose =require("mongoose")
-
+const { MongooseFindByReference } = require('mongoose-find-by-reference');
 const BidSchema=new mongoose.Schema({
-   body:String,
+    additionalInfo:{type:String,required: [true, "there should be additional info"]},
+   counterPrice:{type:Number,required: [true, "we should have a counter offer"]},
 owner:{
     type:mongoose.Types.ObjectId,
     ref:"User"
@@ -11,6 +12,7 @@ owner:{
     ref:"Parfume"
 }
 
-},{timestamps:true})
+})
+BidSchema.plugin(MongooseFindByReference);
 const bid=mongoose.model("bid",BidSchema)
 module.exports=bid
