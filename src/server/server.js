cd09fd62909a983c5e0e320841749559fa969bcd
Pavel-Kolev <146889340+Pavel-Kolev@users.noreply.github.com>
2023-12-09 22:39:57 +0200

    Update server.js

--- src/server/server.js
+++ src/server/server.js
@@ -29,7 +29,7 @@ app.use(errorHandlerMiddleware)
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-	origin: 'http://localhost:5173',
+	origin: 'https://main--incomparable-gumdrop-00bbd4.netlify.app/',
 	credentials: true
 }))
 app.use(cookieParser())
