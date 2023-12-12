78169bda09ea88959872eaf7419d49e46d2d1bb7
Pavel-Kolev <146889340+Pavel-Kolev@users.noreply.github.com>
2023-12-12 09:00:49 +0200

    validatrion and dependancy
    url validation and validator dependancy

--- src/client/utils/validateListing.js
+++ src/client/utils/validateListing.js
@@ -2,14 +2,19 @@ function validateListing(values){
     const {brand,model,img,price,disc}=values
     let errors=[]
     
+const isValidImageUrl = validator.isURL(img, { protocols: ['http', 'https'], require_tld: true });
    
-    if(brand.length<7||model.length<7||img.length<7||disc.length<7)
+    if(brand.length<2||model.length<5||disc.length<10)
     {
     errors.push( "Provide input that is a bit longer" )
     }
     if(Number(price)>1500){
         errors.push( "We are sorry, but such expensive products shoudn't be sold on here " )
     }
+    if(!isValidImageUrl){
+        errors.push("please provide a valid url")
+    }
     return(errors)
     }
+   
     export default validateListing
